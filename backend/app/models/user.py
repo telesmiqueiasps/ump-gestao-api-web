b264488fe10e63e3bc3f76e47eb7a6604950c6fe
Miqueias Teles <miqueiasteles9@gmail.com>
2026-04-17 20:39:30 -0300

    fix: remove relationship UserOrganization do model User

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -13,14 +13,13 @@ class User(Base):
     organization_id = Column(UUID(as_uuid=True), nullable=False)
     organization_type = Column(SAEnum(OrgType, name="org_type", values_callable=lambda x: [e.value for e in x]), nullable=False)
     full_name = Column(String(200), nullable=False)
-    email = Column(String(255), unique=True, nullable=False)
+    email = Column(String(255), nullable=False)
     password_hash = Column(String, nullable=False)
     is_active = Column(Boolean, default=True)
     deactivated_at = Column(DateTime(timezone=True), nullable=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
 
     roles                = relationship("UserRole", back_populates="user")
-    extra_organizations  = relationship("UserOrganization", foreign_keys="UserOrganization.user_id")
 
 
 class UserRole(Base):
